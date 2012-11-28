a39188094a1cf249ef27b96df326136247660633
Genki <cfhoyuk.reccos.nelg@gmail.com>
2012-11-29 07:48:14 +0900

    Add "report-ja" command (copy mmd template in cwd)

---
+++ LaTeX/bin/report-ja.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+TEMPLATES_DIR=$HOME/templates
+
+cp $TEMPLATES_DIR/LaTeX/mmd/my-japanese-report.mmd report.mmd
--- LaTeX/setup.sh
+++ LaTeX/setup.sh
@@ -4,7 +4,12 @@ TEMPLATES_DIR=$HOME/templates
 
 TEXMF=$HOME/texmf
 if [ -e $TEXMF ]; then
-	echo "$TEXMF exists. abort."
-	exit 1
+	echo "$TEXMF exists"
+else
+	ln -s $TEMPLATES_DIR/LaTeX/texmf $TEXMF
 fi
-ln -s $TEMPLATES_DIR/LaTeX/texmf $TEXMF
+
+if [ ! -e $HOME/bin/report-ja ]; then
+	ln -s $TEMPLATES_DIR/LaTeX/bin/report-ja.sh $HOME/bin/report-ja
+fi
+
