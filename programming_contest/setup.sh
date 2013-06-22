31b28674746a5b7804b31823e8c16f900b376c30
Genki Sugimoto <cfhoyuk.reccos.nelg@gmail.com>
2013-06-22 13:16:30 +0900

    Setup aoj working directory

--- programming_contest/setup.sh
+++ programming_contest/setup.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 
 ln -s $HOME/templates/programming_contest/icpc/icpc.sh $HOME/bin/icpc
+dos2unix $HOME/templates/programming_contest/ojh/oj.py
+chmod +x $HOME/templates/programming_contest/ojh/oj.py
