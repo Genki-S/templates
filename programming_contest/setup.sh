2a60368de56b1936c3efa4f8d17bfdfdeee98433
Genki Sugimoto <cfhoyuk.reccos.nelg@gmail.com>
2013-06-24 05:20:38 +0900

    Link oj to $HOME/bin

--- programming_contest/setup.sh
+++ programming_contest/setup.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
 ln -s $HOME/templates/programming_contest/icpc/icpc.sh $HOME/bin/icpc
+
 dos2unix $HOME/templates/programming_contest/ojh/oj.py
 chmod +x $HOME/templates/programming_contest/ojh/oj.py
+ln -s $HOME/templates/programming_contest/ojh/oj.py $HOME/bin/oj
